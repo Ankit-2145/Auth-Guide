7f0a0b565134f37007e65a5a07bc540aa329dc64
Ankit-2145 <ankitsharma2615@gmail.com>
2024-11-19 17:46:06 +0530

    UI Components created

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,28 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { CardWrapper } from "@/components/auth/card-wrapper";
+
+export const LoginForm = () => {
+  const form = useForm();
+
+  return (
+    <CardWrapper
+      headerLabel="Welcome back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      Login Form!
+    </CardWrapper>
+  );
+};
