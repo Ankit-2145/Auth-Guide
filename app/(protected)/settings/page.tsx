01254895a6f516a50b8b036306dccaf40f4c56a3
Ankit-2145 <ankitsharma2615@gmail.com>
2024-11-23 11:17:10 +0530

    signin/signout added

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,9 +1,23 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
 
 const SettingsPage = async () => {
   const session = await auth();
 
-  return <div>{JSON.stringify(session)}</div>;
+  return (
+    <div>
+      {JSON.stringify(session)}
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit" className="bg-sky-400 py-2 px-4 text-white rounded-md">
+          Sign out
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SettingsPage;
